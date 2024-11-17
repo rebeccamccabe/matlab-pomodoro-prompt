db795b8a7e8cc21af1cca5fb0defd61293774386
rebeccamccabe <chessgirl2400@gmail.com>
2024-11-16 23:31:41 -0500

    add green yellow red color

--- taskTracker.m
+++ taskTracker.m
@@ -97,11 +97,27 @@ function drawPomo(timer, taskLabel, duration)
     angle = 2*pi*linspace(0,fractionRemaining,angleResolution);
     x = [0 -sin(angle)];
     y = [0 cos(angle)];
-    fill(hAx, x,y,'g');
-    axis equal
+    fill(hAx, x,y,greenyellowred(fractionRemaining));
+    axis(hAx,'equal')
     xlim(hAx,[-1 1])
     ylim(hAx,[-1 1])
     timeRemaining = datetime(0, 1, 1, 0, 0, duration*fractionRemaining);
     xlabel(hAx,['Time remaining: ' datestr(timeRemaining,'MM:SS')])
     title(hAx,taskLabel)
+end
+
+function greenyellowred = greenyellowred( val )
+    colors =  [ 0  1 0; % green
+                .5 1 0; % yellow-green
+                1  1 0; % yellow
+                1 .5 0; % orange
+                1  0 0];% red
+
+    greenyellowred = [0 0 0];
+    for i=1:3
+        % Interpolate over RGB spaces of colormap
+        temp = interp1(linspace(1,0,length(colors)), colors(:,i), val);
+        greenyellowred(i) = min(max(temp', 0), 1);
+    end
+
 end
